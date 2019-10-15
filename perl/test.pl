f75bd01382c9c3d5b1dcded636ffbdca892953ed
聂伟星 <niewx@ruyi.ai>
2019-10-15 16:39:07 +0800

    更新文件

---
+++ perl/test.pl
@@ -0,0 +1,25 @@
+#!/usr/bin/perl
+use strict;
+use warnings FATAL => 'all';
+
+# sub Hello{
+#     print "Hello, World!\n";
+# }
+#
+# # 函数调用
+# Hello();
+
+
+# 方法定义
+sub PrintHash{
+    my (%hash) = @_;
+
+    foreach my $key ( keys %hash ){
+        my $value = $hash{$key};
+        print "$key : $value\n";
+    }
+}
+my%hash = ('name' => 'runoob', 'age' => 3);
+
+# 传递哈希
+PrintHash(%hash);
